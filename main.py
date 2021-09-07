13f8c6262e0d1df467cfa7d94894c33642146680
Kamina <37193227+Segmentree@users.noreply.github.com>
2021-09-07 19:13:52 +0200

    use and test beautifulsoup

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import requests
+from bs4 import BeautifulSoup
 page = requests.get("https://dataquestio.github.io/web-scraping-pages/simple.html")
 print('Request response: ', page.status_code)
 print('Web page html: ')
 print( page.content)
-
+soup = BeautifulSoup(page.content, 'html.parser')
+print('Soup : ', soup.prettify())
+print('Children : ', list(soup.children))
