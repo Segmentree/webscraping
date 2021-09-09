1ef2d4e269ebb297137f4da9c57da4605ed0fd8a
Kamina <37193227+Segmentree@users.noreply.github.com>
2021-09-09 20:21:57 +0200

    rename files and import vote_lib in the main program

--- main.py
+++ main.py
@@ -1,71 +1,88 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from random import choice
-from string import ascii_lowercase
-import time
-import names
-import os
+import vote_lib
+import logging
 
-URL = 'https://www.korea.net/TalkTalkKorea/English/winners/WIN0000000468'
-OPTIONS = ['--ignore-certificate-errors', '--incognito',
-           '--disable-dev-shm-usage', '--no-sandbox', '--headless']
-# OPTIONS = ['--ignore-certificate-errors', '--incognito']
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
 
-count = 0
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+USERNAME = range(2)
 
 
-def name_generator():
-    return names.get_full_name()
+# Define a few command handlers. These usually take the two arguments update and
+# context. Error handlers also receive the raised TelegramError object in error.
+def start(update, context):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hi! master')
 
 
-def word_generator(n):
-    return ''.join(choice(ascii_lowercase) for i in range(n))
+def help(update, context):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
 
 
-def email_generator():
-    return word_generator(12) + '@' + word_generator(12) + '.com'
+def vote(update, context):
+    """Vote with a custom user"""
+    update.message.reply_text('Enter a username')
+    return USERNAME
 
 
-def add_vote(url=URL):
-    global count
-    NAME = name_generator()
-    EMAIL = email_generator()
-    try:
-        options = webdriver.ChromeOptions()
-        # options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-        for opt in OPTIONS:
-            options.add_argument(opt)
-        # driver = webdriver.Chrome(executable_path=os.environ.get(
-        #     "CHROMEDRIVER_PATH"), options=options)
-        driver = webdriver.Chrome(options=options)
-        driver.get(url)
-        accept = driver.find_element_by_class_name('co-btn--danger')
-        accept.click()
-        time.sleep(1)
-        name = driver.find_element_by_id('voteName')
-        email = driver.find_element_by_id('voteEmail')
-        check = driver.find_element_by_id('countryOth')
-        name.send_keys(NAME)
-        email.send_keys(EMAIL)
-        check.click()
-        submit = driver.find_element_by_xpath(
-            '//a[@href="'+'javascript:fn_egov_save();'+'"]')
-        submit.click()
-        time.sleep(2)
-        success = driver.find_element_by_xpath(
-            "//*[contains(text(), 'Thank you for participating in the voting event!!')]")
-        # confirm = driver.find_element_by_xpath("//*[contains(text(), 'Confirm')]")
-        # confirm.click()
-        if success.text != 'Thank you for participating in the voting event!!':
-            raise ValueError("Doesn't match")
-        print('Success : ', success.text)
-        count += 1
-        driver.close()
-    except:
-        print('Fail')
+def save_username(update, context):
+    update.message.reply_text(
+        f'You have selected {update.message.text} as user name')
 
 
-def engine(iteration_size, url=URL):
-    for i in range(0, iteration_size):
-        add_vote(url)
-    print(f'End proccess { count }')
+def echo(update, context):
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
+
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+
+def main():
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater(
+        "1996037339:AAHFMfNkXMeI-_Da-JBDhqCKazanD4d7hx0", use_context=True)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(
+        ConversationHandler(
+            entry_points=[CommandHandler("vote", vote)],
+            fallbacks=[],
+            states={
+                USERNAME: [MessageHandler(Filters.text, save_username)]
+            }
+        )
+    )
+
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(MessageHandler(Filters.text, echo))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
