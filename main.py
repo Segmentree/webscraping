624dff77e189db82e0001a29cf07736b1e46ab02
Kamina <37193227+Segmentree@users.noreply.github.com>
2021-09-09 14:57:32 +0200

    refactor and organize the current code and add try except behaviour to count success votes

--- main.py
+++ main.py
@@ -5,33 +5,62 @@ from string import ascii_lowercase
 import time
 import names
 
+URL = 'https://www.korea.net/TalkTalkKorea/English/winners/WIN0000000468'
+OPTIONS = ['--ignore-certificate-errors', '--incognito', '--headless']
+# OPTIONS = ['--ignore-certificate-errors', '--incognito']
+count = 0
 
-for i in range(0, 1000):
-    NAME = names.get_full_name()
-    EMAIL = ''.join(choice(ascii_lowercase) for i in range(
-        12)) + '@' + ''.join(choice(ascii_lowercase) for i in range(12)) + '.com'
-    options = webdriver.ChromeOptions()
-    options.add_argument('--ignore-certificate-errors')
-    options.add_argument('--incognito')
-    options.add_argument('--headless')
-    driver = webdriver.Chrome(chrome_options=options)
-    driver.get(
-        "https://www.korea.net/TalkTalkKorea/English/winners/WIN0000000468")
-    accept = driver.find_element_by_class_name('co-btn--danger')
-    accept.click()
-    time.sleep(1)
-    name = driver.find_element_by_id('voteName')
-    email = driver.find_element_by_id('voteEmail')
-    check = driver.find_element_by_id('countryOth')
-    name.send_keys(NAME)
-    email.send_keys(EMAIL)
-    check.click()
-    page_source = driver.page_source
-    submit = driver.find_element_by_xpath(
-        '//a[@href="'+'javascript:fn_egov_save();'+'"]')
-    submit.click()
-    time.sleep(5)
-    driver.close()
-    print(f'vote number { i }')
 
-print('End proccess')
+def name_generator():
+    return names.get_full_name()
+
+
+def word_generator(n):
+    return ''.join(choice(ascii_lowercase) for i in range(n))
+
+
+def email_generator():
+    return word_generator(12) + '@' + word_generator(12) + '.com'
+
+
+def add_vote(url=URL):
+    global count
+    NAME = name_generator()
+    EMAIL = email_generator()
+    try:
+        options = webdriver.ChromeOptions()
+        for opt in OPTIONS:
+            options.add_argument(opt)
+        driver = webdriver.Chrome(options=options)
+        driver.get(url)
+        accept = driver.find_element_by_class_name('co-btn--danger')
+        accept.click()
+        time.sleep(1)
+        name = driver.find_element_by_id('voteName')
+        email = driver.find_element_by_id('voteEmail')
+        check = driver.find_element_by_id('countryOth')
+        name.send_keys(NAME)
+        email.send_keys(EMAIL)
+        check.click()
+        submit = driver.find_element_by_xpath(
+            '//a[@href="'+'javascript:fn_egov_save();'+'"]')
+        submit.click()
+        time.sleep(2)
+        success = driver.find_element_by_xpath(
+            "//*[contains(text(), 'Thank you for participating in the voting event!!')]")
+        # confirm = driver.find_element_by_xpath("//*[contains(text(), 'Confirm')]")
+        # confirm.click()
+        if success.text != 'Thank you for participating in the voting event!!':
+            raise ValueError("Doesn't match")
+        print('Success : ', success.text)
+        count += 1
+        driver.close()
+    except:
+        print('Fail')
+
+
+for i in range(1, 2000):
+    add_vote()
+    print(f'cycle : {i}')
+
+print(f'End proccess { count }')
